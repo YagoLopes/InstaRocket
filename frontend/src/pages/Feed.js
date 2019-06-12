76a101ffc9167326bbd4940a6f1c59500f27b0f6
Yago Lopes <yagolopeslazaro@gmail.com>
2019-06-12 16:28:10 -0300

    frontend inicial commit

---
+++ frontend/src/pages/Feed.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+// import { Container } from './styles';
+
+export default class Feed extends Component {
+  render() {
+    return <h1>Feed</h1>;
+  }
+}
---
+++ frontend/src/pages/New.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+// import { Container } from './styles';
+
+export default class New extends Component {
+  render() {
+    return <h1>New</h1>;
+  }
+}
