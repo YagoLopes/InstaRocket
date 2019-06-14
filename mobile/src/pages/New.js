81ef9fa4ee27a7b0c64d038f0259b96910f78efc
Yago Lopes <yagolopeslazaro@gmail.com>
2019-06-14 16:07:05 -0300

    finalizando a aplicação

--- mobile/src/pages/New.js
+++ mobile/src/pages/New.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import api from "../services/api";
 import ImagePicker from "react-native-image-picker";
 import styles from "./NewStyles";
 import { View, TouchableOpacity, Text, TextInput, Image } from "react-native";
@@ -7,14 +8,62 @@ export default class New extends Component {
   static navigationOptions = {
     headerTitle: "Nova publicação"
   };
-  state = { author: "", place: "", description: "", hashtags: "" };
+  state = {
+    preview: null,
+    image: null,
+    author: "",
+    place: "",
+    description: "",
+    hashtags: ""
+  };
+
+  handleSubmit = async () => {
+    const data = new FormData();
+
+    data.append("image", this.state.image);
+    data.append("author", this.state.author);
+    data.append("place", this.state.place);
+    data.append("description", this.state.description);
+    data.append("hashtags", this.state.hashtags);
+
+    await api.post("/posts", data);
+    this.props.navigation.navigate("Feed");
+  };
 
   handleSelectImage = () => {
     ImagePicker.showImagePicker(
       {
         title: "Selectionar imagem"
       },
-      upload => {}
+      upload => {
+        if (upload.error) {
+          console.log("Error");
+        } else if (upload.didCancel) {
+          console.log("Used canceled");
+        } else {
+          const preview = {
+            uri: `data:image/jpeg;base64,${upload.data}`
+          };
+          let prefix;
+          let ext;
+
+          if (upload.fileName) {
+            [prefix, ext] = upload.fileName.split(".");
+            ext = ext.toLocaleLowerCase() === "heic" ? "jpg" : ext;
+          } else {
+            prefix = new Date().getTime();
+            ext = "jpg";
+          }
+
+          const image = {
+            uri: upload.uri,
+            type: upload.type,
+            name: `${prefix}.${ext}`
+          };
+
+          this.setState({ preview, image });
+        }
+      }
     );
   };
 
@@ -27,6 +76,11 @@ export default class New extends Component {
         >
           <Text style={styles.selectButtonText}>Selecionar imagem</Text>
         </TouchableOpacity>
+
+        {this.state.preview && (
+          <Image style={styles.preview} source={this.state.preview} />
+        )}
+
         <TextInput
           style={styles.input}
           autoCapitalize="none"
@@ -34,7 +88,7 @@ export default class New extends Component {
           placeholder="Nome do autor"
           placeholderTextColor="#999"
           value={this.state.author}
-          onChangeText={text => this.setState({ author })}
+          onChangeText={text => this.setState({ author: text })}
         />
         <TextInput
           style={styles.input}
@@ -43,7 +97,7 @@ export default class New extends Component {
           placeholder="Local da foto"
           placeholderTextColor="#999"
           value={this.state.place}
-          onChangeText={text => this.setState({ place })}
+          onChangeText={text => this.setState({ place: text })}
         />
         <TextInput
           style={styles.input}
@@ -52,7 +106,7 @@ export default class New extends Component {
           placeholder="Descrição"
           placeholderTextColor="#999"
           value={this.state.description}
-          onChangeText={text => this.setState({ description })}
+          onChangeText={text => this.setState({ description: text })}
         />
         <TextInput
           style={styles.input}
@@ -61,9 +115,12 @@ export default class New extends Component {
           placeholder="Hashtags"
           placeholderTextColor="#999"
           value={this.state.hashtags}
-          onChangeText={text => this.setState({ hashtags })}
+          onChangeText={text => this.setState({ hashtags: text })}
         />
-        <TouchableOpacity style={styles.shareButton} onPress={() => {}}>
+        <TouchableOpacity
+          style={styles.shareButton}
+          onPress={this.handleSubmit}
+        >
           <Text style={styles.shareButtonText}>Compartilhar</Text>
         </TouchableOpacity>
       </View>
--- mobile/src/routes.js
+++ mobile/src/routes.js
@@ -12,7 +12,6 @@ export default createAppContainer(
       New
     },
     {
-      initialRouteName: "New",
       defaultNavigationOptions: {
         headerTintColor: "#000",
         headerTitle: <Image style={{ marginHorizontal: 20 }} source={logo} />,
